b1cee41bd6bfd3dc2af75be8334558a734801e20
Christoph Stephan <chm.stephan@outlook.com>
2019-02-10 13:07:11 +0100

    implement twist controller very similar to the solution in the course

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,4 +1,7 @@
 from yaw_controller import YawController
+from pid import PID
+from lowpass import LowPassFilter
+import rospy
 
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
@@ -26,7 +29,46 @@ class Controller(object):
             self.max_lat_accel,
             self.max_steer_angle)
 
-    def control(self, *args, **kwargs):
+        kp = 0.3
+        ki = 0.1
+        kd = 0.
+        mn = 0. # Minimum throttle value
+        mx = 0.2 # Maximum throttle value
+        self.throttle_controller = PID(kp, ki, kd, mn, mx)
+
+        tau = 0.5 # 1/(2pi*tau) = cutoff frequency
+        ts = .02 # Sample time
+        self.vel_lpf = LowPassFilter(tau, ts)
+
+        self.last_time = rospy.get_time()
+
+    def control(self, current_vel, dbw_enabled, linear_vel, angular_vel):
         # TODO: Change the arg, kwarg list to suit your needs
         # Return throttle, brake, steer
-        return 1., 0., 0.
+
+        if not dbw_enabled:
+            self.throttle_controller.reset()
+            return 0., 0., 0.
+
+        current_vel = self.vel_lpf.filt(current_vel)
+
+        steering = self.yaw_controller.get_steering(linear_vel, angular_vel, current_vel)
+
+        vel_error = linear_vel - current_vel
+
+        current_time = rospy.get_time()
+        sample_time = current_time - self.last_time
+        self.last_time = current_time
+
+        throttle = self.throttle_controller.step(vel_error, sample_time)
+        brake = 0.
+
+        if abs(linear_vel) < 0.0001 and abs(current_vel) < 0.1:
+            throttle = 0.
+            brake = 700 # Nm - to hold the car in place if we are stopped at a light. Acceleration - 1m/s^2
+        elif throttle < 0.1 and vel_error < 0.:
+            throttle = 0.
+            decel = max(vel_error, self.decel_limit)
+            brake = -1. * decel * self.vehicle_mass * self.wheel_radius # Nm - Torque
+
+        return throttle, brake, steering
